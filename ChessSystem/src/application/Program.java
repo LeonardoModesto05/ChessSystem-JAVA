5c8c63096c3203c8ea3b4aa43604d186dbcb3157
Leonardo Modesto <leonardoaparecido216@gmail.com>
2024-03-13 00:28:18 -0300

    First Class: positions

--- ChessSystem/src/application/Program.java
+++ ChessSystem/src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
+import boardgame.Positions;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Hello World!");
+		Positions postions = new Positions(3,5);
+		
+		System.out.println(postions);
 
 	}
 
